7ee45ca467691fdbeabc1775eae6630c8fe8a3c2
timestep <aaronksivbalan@gmail.com>
2013-06-05 12:16:12 -0400

    No

--- hangman.rb
+++ hangman.rb
@@ -1,68 +1,70 @@
-#set the list of random words
-words = ["lemonade", "apple", "phone", "soda"]
+require 'pry'
 
-#set answer array 
-word = words[rand(4)]
-answer = word.split("")
+	#set the list of random words
+	words = ["lemonade", "apple", "phone", "soda"]
 
-#set guessed array
-guess = []
-word.length.times do
-	guess = guess + ["_"]
-end
-puts guess.join(" ")
+	#set answer array 
+	word = words[rand(4)]
+	answer = word.split("")
 
-#set array for used letters
-$used = []
-def send_to_used(used_letter)
-	if $used.index(used_letter)
-		puts "You already used that letter."
-	else
-		$used << used_letter
-		$used = $used.sort
+	#set guessed array
+	guess = []
+	word.length.times do
+		guess = guess + ["_"]
 	end
-end
+	puts guess.join(" ")
 
-#interactive part
-win = 8
-i =0
-while true 
-	puts
-	puts
-
-	letter_guess = gets.chomp.downcase
-	send_to_used(letter_guess)
-
-	check = false
-
-	0.upto(word.length-1) do |x|
-		if answer[x] == letter_guess
-			guess[x] = letter_guess
-			check = true
+	#set array for used letters
+	$used = []
+	def send_to_used(used_letter)
+		if $used.index(used_letter)
+			puts "You already used that letter."
+		else
+			$used << used_letter
+			$used = $used.sort
 		end
 	end
 
-	if !check
-		i += 1
+	#interactive part
+	win = 8
+	i =0
+	while true 
+		puts
+		puts
+
+		letter_guess = gets.chomp.downcase
+		send_to_used(letter_guess)
+
+		check = false
+
+		0.upto(word.length-1) do |x|
+			if answer[x] == letter_guess
+				guess[x] = letter_guess
+				check = true
+			end
+		end
+
+		if !check
+			i += 1
+		end
+
+		puts guess.join(" ")
+
+		if guess == answer
+			puts "You won!"
+			break
+		end
+
+		puts "These are your used letters: #{$used.join(", ")}"
+
+		puts "You have #{win-i} guesses left."
+
+		if i == win
+			puts "You lost! Sorry but you suck."
+			break
+		end
+
+		puts
+		puts
+
 	end
-
-	puts guess.join(" ")
-
-	if guess == answer
-		puts "You won!"
-		break
-	end
-
-	puts "These are your used letters: #{$used.join(", ")}"
-
-	puts "You have #{win-i} guesses left."
-
-	if i == win
-		puts "You lost! Sorry but you suck."
-		break
-	end
-
-	puts
-	puts
-
-end
